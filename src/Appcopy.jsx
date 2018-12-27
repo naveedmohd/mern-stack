44aff75b58ea35800e6f7e25bf645a73a003d0d5
Mohammad Naveed <mnaveed@mohammads-mbp-2.attlocal.net>
2018-12-27 09:23:43 -0800

    FirstCommit

---
+++ src/Appcopy.jsx
@@ -0,0 +1,3 @@
+var contentNode = document.getElementById('contents');
+var component = <h1>Hello World Today2!</h1>;        // A simple component, written in JSX
+ReactDOM.render(component, contentNode);      // Render the component inside the content Node
